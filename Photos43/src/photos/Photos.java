bb84023e8dc98557739d4c7d24819239d6372706
Deep Kotadia <deep.kotadia@rutgers.edu>
2017-11-13 18:00:03 -0500

    Photos.java has JavaFX functionality

--- Photos43/src/photos/Photos.java
+++ Photos43/src/photos/Photos.java
@@ -1,9 +1,28 @@
 package photos;
 
-public class Photos {
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import java.util.*;
+
+
+public class Photos extends Application {
+	
+	Stage mainStage;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		launch(args);
 
 	}
 
