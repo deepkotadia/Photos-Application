406e0d197a4c6c0862cc3de05f884e844ca7e81f
chinmoyi <cb849@scarletmail.rutgers.edu>
2017-11-19 13:48:44 -0500

    working on search by date

--- Photos43/src/control/SearchControl.java
+++ Photos43/src/control/SearchControl.java
@@ -1,5 +1,6 @@
 package control;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import javafx.event.ActionEvent;
@@ -7,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -30,6 +32,8 @@ import model.Photo;
 public class SearchControl implements LogoutInterface {
 	@FXML Button dateSearch, tagSearch, createAlbum, logoutBtn;
 	
+	@FXML DatePicker startDate, endDate;
+	
 	@FXML ListView<Photo> photosList;
 	
 	@FXML TextArea detailBox;
@@ -44,24 +48,59 @@ public class SearchControl implements LogoutInterface {
 	 * Displays Dialog box for giving date range to search for
 	 */
 	public void handleSearchByDate(ActionEvent event) {
-		   Dialog<Album> dialog = new Dialog<>();
+		   Dialog<LocalDate> dialog = new Dialog<>();
 		   dialog.setTitle("Search for photos by Date");
 		   dialog.setHeaderText("Start Date");
 		   dialog.setResizable(true);
 		   
 		   Label startDateLabel = new Label("Start Date: ");
-		   //TextField albumnameTextField = new TextField();
+		   startDate.getValue();
 		   Label endDateLabel = new Label("End Date: ");
+		   endDate.getValue();
 		   
 		   GridPane grid = new GridPane();
 		   grid.add(startDateLabel, 1, 1);
-		   //grid.add(albumnameTextField, 2, 1);
+		   grid.add(startDate, 2, 1);
 		   grid.add(endDateLabel, 2, 1);
+		   grid.add(endDate, 2, 2);
 		   
 		   dialog.getDialogPane().setContent(grid);
 		   
 		   ButtonType buttonTypeOk = new ButtonType("OK", ButtonData.OK_DONE);
 		   dialog.getDialogPane().getButtonTypes().add(buttonTypeOk);
+		   
+		   dialog.setResultConverter(new Callback<ButtonType, LocalDate>() {
+			   @Override
+			   public LocalDate call(ButtonType b) {
+				   if (b == buttonTypeOk) {
+					   
+					  // String error = checkFields();
+					   
+					  /*if (error != null) {
+						   Alert alert = new Alert(AlertType.ERROR);
+						   alert.setTitle("Error Dialog");
+						   alert.setHeaderText(error);
+						   alert.setContentText("Please try again");
+
+						   Optional<ButtonType> buttonClicked=alert.showAndWait();
+						   if (buttonClicked.get()==ButtonType.OK) {
+							   alert.close();
+						   }
+						   else {*/
+							   //alert.close();
+						  // }
+						   //return null;
+					   //}
+											   
+					  // return new LocalDate(	);
+				   }
+				   return null;
+			   }
+			
+		   });
+		   
+		   //wait for response from add button
+		   dialog.showAndWait();
 		   
 		  
 	}
